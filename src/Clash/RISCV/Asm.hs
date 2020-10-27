a7bf4c301886a20c8b7ec595c6ff95924c231b41
Øystein Hovind <hovindo@gmail.com>
2020-10-27 19:30:15 +0100

    Add some assembly

--- src/Clash/RISCV/Asm.hs
+++ src/Clash/RISCV/Asm.hs
@@ -5,5 +5,20 @@ module Clash.RISCV.Asm where
 import Clash.Prelude
 import Clash.RISCV
 
+
+hi :: Addr -> BitVector 20
+hi = slice d31 d12
+
+lo :: Addr -> BitVector 12
+lo = slice d11 d0
+
+x0, x1, x2 :: Register
+x0 = 0
+x1 = 1
+x2 = 2
+
 addi :: Register -> Register -> BitVector 12 -> Instr
 addi = Imm AddI
+
+nop :: Instr
+nop = addi x0 x0 0
