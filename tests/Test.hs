ef4f2ad73c856e911af55bc663180bde67e189b2
Øystein Hovind <hovindo@gmail.com>
2021-06-18 20:29:37 +0200

    Fix tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,22 +1,28 @@
 import Prelude
-import Control.Monad
-import System.Directory
+import Control.Monad (filterM)
+import Data.List (isPrefixOf, isSuffixOf)
+import System.Directory (doesFileExist, getCurrentDirectory, listDirectory)
 import Test.Tasty
 
 import qualified Tests.Clash.RISCV.Decode
 import qualified Tests.Clash.RISCV.Spec
 
-getTests :: IO [FilePath]
-getTests = do
-  dir <- getCurrentDirectory
-  files <- filter rv32ui <$> listDirectory dir
+infixr 5  </>
+(</>) :: FilePath -> FilePath -> FilePath
+a </> b = a ++ "/" ++ b
+
+getTests :: FilePath -> IO [FilePath]
+getTests dir = do
+  files <- fmap prefix . filter rv32ui <$> listDirectory dir
   filterM doesFileExist files
     where
-      rv32ui = const True
+      rv32ui base = "rv32ui-p" `isPrefixOf` base && not (".dump" `isSuffixOf` base)
+      prefix file = dir </> file
 
 main :: IO ()
 main = do
-  tree <- getTests
+  dir <- getCurrentDirectory
+  tree <- getTests $ dir </> "riscv-tests" </> "isa"
   defaultMain $ testGroup "."
     [ Tests.Clash.RISCV.Decode.tests
     , Tests.Clash.RISCV.Spec.tests tree
